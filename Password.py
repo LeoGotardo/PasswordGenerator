f3455e45d8d9610f4fc4829ac492d923b4511365
Y0nlu <leonardo.gotardo9@gmail.com>
2024-01-17 13:16:55 -0300

    1

--- Password.py
+++ Password.py
@@ -1,31 +1,33 @@
-import os
 import random as r
+import pyperclip
+import os
+
 
 def main():
     cls = os.system("cls")
-    tam = int(input(print(f"Quantos digitos deve ter sua senha?\n")))
-    num = input(print(f"Deve ter numeros? S/N\n"))
-    letter = input(print(f"Deve ter letras? S/N\n"))
-    symb = input(print(f"Deve ter simbulos? S/N\n"))
-    maius = input(print(f"Deve ter letras maiusculas? S/N\n"))
+    tam = int(input(f"Quantos digitos deve ter sua senha?\n"))
+    num = input(f"Deve ter numeros? S/N\n")
+    letter = input(f"Deve ter letras? S/N\n")
+    symb = input(f"Deve ter simbulos? S/N\n")
+    maius = input(f"Deve ter letras maiusculas? S/N\n")
 
     let = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     may = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    sim = ['!','@','#','$','%','¨','&','*','(',')','+','=','|','<','>',':',';','?']
     numb = ['0','1','2','3','4','5','6','7','8','9']
-    sim = ['ç','!','@','#','$','%','¨','&','*','(',')','+','=','|','<','>',':',';','?']
-    fin = []
     password = []
+    fin = []
 
-    if num == "S" or num == "s":
+    if num.lower() == "s":
         for i in range(len(numb)):
             fin.append(numb[i])
-    if letter == "S" or letter == "s":
+    if letter.lower() == "s":
         for i in range(len(let)):
             fin.append(let[i])
-    if symb == "S" or symb == "s":
+    if symb.lower() == "s":
         for i in range(len(sim)):
             fin.append(sim[i])
-    if maius == "S" or maius == "s":
+    if maius.lower() == "s":
         for i in range(len(may)):
             fin.append(may[i])
     for i in range(tam):
@@ -36,6 +38,7 @@ def main():
 
     cls
     
-    print(F"Sua senha é: {fin_pass}")
+    pyperclip.copy(fin_pass)
+    print(F"Sua senha copiada a sua clipbord.")
 
 main()
