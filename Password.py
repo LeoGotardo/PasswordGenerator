4e40a4f5c7d15f56215ae8c2a5bc4da5cb077f20
Leonardo Gotardo <125305856+LeoGotardo@users.noreply.github.com>
2024-02-12 13:27:07 -0300

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,5 +1,4 @@
 from alive_progress import alive_bar, config_handler
-from alive_progress import *
 import random as r
 import Debug as d
 import pyperclip
@@ -9,17 +8,20 @@ import os
 def main():
     cls = os.system("cls")
     config_handler.set_global(length=50)
+    
     tam = int(input(f"{d.Margin}Quantos digitos deve ter sua senha?\n"))
     num = input(f"{d.Margin}Deve ter numeros? S/N\n")
     letter = input(f"{d.Margin}Deve ter letras? S/N\n")
     symb = input(f"{d.Margin}Deve ter simbolos? S/N\n")
     maius = input(f"{d.Margin}Deve ter letras maiusculas? S/N\n")
+    
     print(d.Margin)
 
     let = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     may = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     sim = ['!','@','#','$','%','¨','&','*','(',')','+','=','|','<','>',':',';','?']
     numb = ['0','1','2','3','4','5','6','7','8','9']
+    
     password = []
     fin = []
 
@@ -35,11 +37,11 @@ def main():
     if maius.lower() == "s":
         for i in range(len(may)):
             fin.append(may[i])
+            
     with alive_bar(tam, title="Generating your password...") as bar:
         for i in range(tam):
             act = r.choice(fin)
             password.append(act)
-            d.Green
             bar()
     fin_pass = ''.join(password)
 
